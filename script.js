4c372a458e824a3b27dc27467529317e089b3dc6
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-09-20 21:50:32 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function myFunction() {
-    myVar = setTimeout(showPage, 500);
     const kepek = ["./kepek/hatter.jpeg","./kepek/hatter2.jpeg","./kepek/hatter3.jpg","./kepek/hatter4.jpg","./kepek/hatter5.jpg",
     "./kepek/hatter6.jpeg","./kepek/hatter7.jpg","./kepek/hatter8.png",];
   
@@ -11,11 +10,3 @@ function myFunction() {
   const header = document.querySelector("header");
   header.style.backgroundImage = `url(${kep})`;
 }
-function showPage() {
-    var betolto = document.getElementById("betolto");
-    var egesz =   document.getElementById("egesz");
-    betolto.classList.add("hide");
-    egesz.classList.add("aktiv");
-  document.body.style.overflow = "visible";
-  }
-  
