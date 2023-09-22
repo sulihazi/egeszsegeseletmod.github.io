1bbc1bed38c5fa77f222f251a5dd4564b671ae3a
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-09-22 22:53:32 +0200

    .

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function myFunction() {
   const header = document.querySelector("header");
   header.style.backgroundImage = `url(${kep})`;
 }
+
+// Véletlenszerű szövegek és linkek
+const randomTexts = [
+  { text: 'BMI Kalkulátor', link: 'bmikalkulator.html' },
+  { text: 'Vízfogyasztás', link: 'vizfogyasztas.html' },
+  { text: 'Diéták', link: '#' },
+  { text: 'Testmozgás', link: '#' },
+  { text: 'Mentális Egészség', link: '#' },
+  { text: 'Életmódtippek', link: '#' },
+];
+
+// Függvény a gomb módosítására
+function changeButton() {
+  // Véletlenszerű szöveg és link kiválasztása
+  const randomIndex = Math.floor(Math.random() * randomTexts.length);
+  const randomButtonText = randomTexts[randomIndex].text;
+  const randomButtonLink = randomTexts[randomIndex].link;
+
+  // Gomb elem kiválasztása és módosítása
+  const button = document.getElementById('randomGomb');
+  button.textContent = randomButtonText;
+  button.href = randomButtonLink;
+}
+
+// Oldal betöltésekor hívjuk meg a függvényt
+window.addEventListener('load', changeButton);
